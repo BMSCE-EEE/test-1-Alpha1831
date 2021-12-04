6c2149e9b7239f0d17b4013ad639d10ca56d466b
Anul Suvarna <94944814+Alpha1831@users.noreply.github.com>
2021-12-04 02:58:33 +0000

    Code.5

--- p5original.c
+++ p5original.c
@@ -1,49 +1,24 @@
 #include<stdio.h>
-int cmp(int a, int b, int c);
-int input ();
-void comp(int *a, int *b, int *c, int *large);
-void in(int *a, int *b,int *c);
+void cmp(int *a, int *b, int *c, int *l);
+void input(int *a, int *b,int *c);
 int main()
 {
-  int a,b,c,large,A,B,C,L;
-  a=input();
-  b=input();
-  c=input();
-  large=cmp(a,b,c);
-  printf("Largest of the three numbers is %d",large);
-  in(&A,&B,&C);
-  comp(&A,&B,&C,&L);
+  int A,B,C,L;
+  input(&A,&B,&C);
+  cmp(&A,&B,&C,&L);
   printf("Largest of the three numbers is %d",L);
   return 0;
 }
-int input()
+void input(int *a, int *b,int *c)
 {
-  int x;
-  scanf("%d",&x);
-  return x;
+  scanf("%d %d %d",a,b,c);
 }
-int cmp(int a, int b, int c)
+void cmp(int *a, int *b, int *c, int *l)
 {
-  int l;
-  if(a>b && a>c)
-  l=a;
-  else if(b>c)
-  l=b;
-  else
-  l=c;
-  return l;
-}
-void in(int *a, int *b,int *c)
-{
-  scanf("%d %d %d",*a,*b,*c);
-}
-void comp(int *a, int *b, int *c, int *large)
-{
-  int l;
   if(*a>*b && *a>*c)
-  *l=*a;
-  else if(*b>*c)
-  *l=*b;
+  {*l=*a;}
+  else if(*b>*c && *b>*a)
+  {*l=*b;}
   else
-  *l=*c;
+  {*l=*c;}
 }
